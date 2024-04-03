7da46841aecd62fe76f8828e8789edd4a9bd8e92
RodrigoNatalio <74545019+RodrigoNatalio@users.noreply.github.com>
2024-04-03 15:29:00 +0100

    Nível 1

---
+++ AirTransportation.java
@@ -0,0 +1,30 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String name;
+    private int numberOfContainers;
+    private static final double FEES = 4.0;
+
+    /**
+     * Construtor para objetos da classe AirTransportation
+     */
+    public AirTransportation()
+    {
+        // inicializa variáveis de instância
+       
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,29 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String liceensePlate;
+    private static final double FEES = 3.0;
+
+    /**
+     * Construtor para objetos da classe GroundTransportation
+     */
+    public GroundTransportation()
+    {
+        // inicializa variáveis de instância
+        
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    
+}
